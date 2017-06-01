f7911469611f86068d1cd8178c334b055a318270
Tomasz Modelski <OrdoRerumDev@gmail.com>
2017-06-02 00:08:38 +0200

    FightForSurvival : Construction Time & Cost bonuses, small clean ups

--- modules/mtwModPatcher.sship/src/tm/mtwModPatcher/sship/global/FightForSurvival.java
+++ modules/mtwModPatcher.sship/src/tm/mtwModPatcher/sship/global/FightForSurvival.java
@@ -13,7 +13,9 @@ import tm.mtwModPatcher.lib.managers.UnitsManager;
 import java.util.List;
 import java.util.UUID;
 
-/** Smaller factions get survival bonuses */
+/**
+ * Smaller factions get survival bonuses
+ */
 public class FightForSurvival extends Feature {
 
 	@Override
@@ -31,60 +33,58 @@ public class FightForSurvival extends Feature {
 		List<Tuple2<String, List<Integer>>> moneyBonuses;
 
 		String requires;
-		//construction_cost_bonus_stone bonus 90 requires event_counter is_the_ai 1
-		//construction_cost_bonus_wooden bonus 90 requires event_counter is_the_ai 1
-		String constructionStoneBonus = "        construction_cost_bonus_stone bonus ";
-		String constructionWoodenBonus = "        construction_cost_bonus_wooden bonus ";
+
 		// ###### WARNING Level #######
-		requires = " requires event_counter "+ EVENT_WARNING_NAME +" 1";
+		requires = " requires event_counter " + EVENT_WARNING_NAME + " 1";
 		// last : 0.5
-		moneyBonuses = exportDescrBuilding.addFlatCityCastleIncome(requires , 0.75);	// Bonuses : [250 , 375 , 562 , 843 , 1264] with factor 1.0
+		moneyBonuses = exportDescrBuilding.addFlatCityCastleIncome(requires, 0.75);    // Bonuses : [250 , 375 , 562 , 843 , 1264] with factor 1.0
 		consoleLogger.writeLine(Ctm.msgFormat("[{0}]: {1} Flat money bonus: {2}[{3}], {4}[{5}]",
 				FEATURE_NAME, EVENT_WARNING_NAME,
 				moneyBonuses.get(0).getItem1(), Ctm.toCsv(moneyBonuses.get(0).getItem2()),
 				moneyBonuses.get(1).getItem1(), Ctm.toCsv(moneyBonuses.get(1).getItem2())));
 
 		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        law_bonus bonus 1" + requires);
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities(constructionStoneBonus + 5 + requires);
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities(constructionWoodenBonus + 5 + requires);
+
+		insertConstructionCostBonus(5, requires);
+		insertConstructionTimeBonus(10, requires);
 
 
 		// ###### DANGER Level #######
-		requires = " requires event_counter "+ EVENT_DANGER_NAME +" 1";
+		requires = " requires event_counter " + EVENT_DANGER_NAME + " 1";
 
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        law_bonus bonus 2" + requires);					// +20 % Law
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        population_growth_bonus bonus 2" + requires);	// +0.5 % Pop Growth
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        trade_base_income_bonus bonus 1" + requires);	// +1 Trade bonus
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        religion_level bonus 1" + requires);			// +1 Religion
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        recruits_morale_bonus bonus 1" + requires);		// +1 Morale
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        recruits_exp_bonus bonus 1" + requires);		// +1 Experience
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        law_bonus bonus 2" + requires);                    // +20 % Law
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        population_growth_bonus bonus 2" + requires);    // +0.5 % Pop Growth
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        trade_base_income_bonus bonus 1" + requires);    // +1 Trade bonus
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        religion_level bonus 1" + requires);            // +1 Religion
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        recruits_morale_bonus bonus 1" + requires);        // +1 Morale
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        recruits_exp_bonus bonus 1" + requires);        // +1 Experience
 
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        free_upkeep bonus 1" + requires);				// +2 Free Upkeep
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities(constructionStoneBonus + 10 + requires);
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities(constructionWoodenBonus + 10 + requires);
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        free_upkeep bonus 1" + requires);                // +2 Free Upkeep
+		insertConstructionCostBonus(10, requires);
+		insertConstructionTimeBonus(6, requires);
 
-		moneyBonuses = exportDescrBuilding.addFlatCityCastleIncome(requires , 1.25 );	// last 1.0
+		moneyBonuses = exportDescrBuilding.addFlatCityCastleIncome(requires, 1.25);    // last 1.0
 		consoleLogger.writeLine(Ctm.msgFormat("[{0}]: {1} Flat money bonus: {2}[{3}], {4}[{5}]",
 				FEATURE_NAME, EVENT_DANGER_NAME,
 				moneyBonuses.get(0).getItem1(), Ctm.toCsv(moneyBonuses.get(0).getItem2()),
 				moneyBonuses.get(1).getItem1(), Ctm.toCsv(moneyBonuses.get(1).getItem2())));
 
 		// ###### CRITICAL Level #######
-		requires = " requires event_counter "+ EVENT_CRITICAL_NAME +" 1";
+		requires = " requires event_counter " + EVENT_CRITICAL_NAME + " 1";
 
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        law_bonus bonus 4" + requires);					// +20 % Law
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        population_growth_bonus bonus 3" + requires);	// +1 % Pop Growth
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        trade_base_income_bonus bonus 2" + requires);	// +2 Trade bonus
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        religion_level bonus 2" + requires);			// +2 Religion
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        recruits_morale_bonus bonus 2" + requires);		// +2 Morale
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        recruits_exp_bonus bonus 2" + requires);		// +2 Experience
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        free_upkeep bonus 2" + requires);				// +2 Free Upkeep
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        law_bonus bonus 4" + requires);                    // +20 % Law
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        population_growth_bonus bonus 3" + requires);    // +1 % Pop Growth
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        trade_base_income_bonus bonus 2" + requires);    // +2 Trade bonus
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        religion_level bonus 2" + requires);            // +2 Religion
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        recruits_morale_bonus bonus 2" + requires);        // +2 Morale
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        recruits_exp_bonus bonus 2" + requires);        // +2 Experience
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        free_upkeep bonus 2" + requires);                // +2 Free Upkeep
 		// +1 Recruitment slot
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        recruitment_slots 1 requires not event_counter freeze_recr_pool 1 and event_counter "+ EVENT_CRITICAL_NAME +" 1");
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities(constructionStoneBonus + 20 + requires);
-		exportDescrBuilding.insertIntoCityCastleWallsCapabilities(constructionWoodenBonus + 20 + requires);
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("        recruitment_slots 1 requires not event_counter freeze_recr_pool 1 and event_counter " + EVENT_CRITICAL_NAME + " 1");
+		insertConstructionCostBonus(20, requires);
+		insertConstructionTimeBonus(4, requires);
 
-		moneyBonuses = exportDescrBuilding.addFlatCityCastleIncome(requires , 2.0);
+		moneyBonuses = exportDescrBuilding.addFlatCityCastleIncome(requires, 2.0);
 		consoleLogger.writeLine(Ctm.msgFormat("[{0}]: {1} Flat money bonus: {2}[{3}], {4}[{5}]",
 				FEATURE_NAME, EVENT_CRITICAL_NAME,
 				moneyBonuses.get(0).getItem1(), Ctm.toCsv(moneyBonuses.get(0).getItem2()),
@@ -93,15 +93,34 @@ public class FightForSurvival extends Feature {
 		unitsManager.enableFreeUpkeepAllUnits(null, exportDescrUnit);
 	}
 
+	private void insertConstructionCostBonus(int bonus, String requires) {
+		//construction_cost_bonus_stone bonus 90 requires event_counter is_the_ai 1
+		//construction_cost_bonus_wooden bonus 90 requires event_counter is_the_ai 1
+		String constructionStoneBonus = "        construction_cost_bonus_stone bonus ";
+		String constructionWoodenBonus = "        construction_cost_bonus_wooden bonus ";
+
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities(constructionStoneBonus + bonus + requires);
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities(constructionWoodenBonus + bonus + requires);
+	}
+	private void insertConstructionTimeBonus(int turnsBonus, String requires) {
+
+		int bonus = (int) ((100.0 / (double)turnsBonus) +1);
+
+		// #### CONSTRUCTION TIMES ######
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("construction_time_bonus_other bonus " + bonus + requires);
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("construction_time_bonus_religious bonus " + bonus + requires);
+		exportDescrBuilding.insertIntoCityCastleWallsCapabilities("construction_time_bonus_defensive bonus " + bonus + requires);
+	}
+
 	protected void createMonitorScripts() throws PatcherLibBaseEx {
-		int insertLine = campaignScript.getLines().findExpFirstRegexLine("^\\s*wait_monitors")-1;
+		int insertLine = campaignScript.getLines().findExpFirstRegexLine("^\\s*wait_monitors") - 1;
 
-		String str="";
+		String str = "";
 
-		str += nl+nl;
-		str += "; ### Fight for survival - faction scripts ###" + nl+nl;
+		str += nl + nl;
+		str += "; ### Fight for survival - faction scripts ###" + nl + nl;
 
-		str += getMonitorForFactions(FactionsDefs.allFactionsList() , 5 , 3 , 1);
+		str += getMonitorForFactions(FactionsDefs.allFactionsList(), 5, 3, 1);
 
 		campaignScript.getLines().insertAt(insertLine, str);
 	}
@@ -116,30 +135,30 @@ public class FightForSurvival extends Feature {
 		return str;
 	}
 
-	protected String getMonitorForFaction(String factionName, int settlementsCountWarning, int settlementCountDanger , int settlementCountCritical) {
-		String str="";
+	protected String getMonitorForFaction(String factionName, int settlementsCountWarning, int settlementCountDanger, int settlementCountCritical) {
+		String str = "";
 
-		str += "monitor_event PreFactionTurnStart FactionType " + factionName +nl+nl;
+		str += "monitor_event PreFactionTurnStart FactionType " + factionName + nl + nl;
 
-		str += "    set_event_counter " + EVENT_WARNING_NAME + " 0" +nl;
-		str += "    set_event_counter " + EVENT_DANGER_NAME + " 0" +nl;
-		str += "    set_event_counter " + EVENT_CRITICAL_NAME + " 0" +nl+nl;
+		str += "    set_event_counter " + EVENT_WARNING_NAME + " 0" + nl;
+		str += "    set_event_counter " + EVENT_DANGER_NAME + " 0" + nl;
+		str += "    set_event_counter " + EVENT_CRITICAL_NAME + " 0" + nl + nl;
 
-		str += "  if I_NumberOfSettlements "+factionName + " <= " + settlementsCountWarning +nl;
-		str += "   and I_NumberOfSettlements "+factionName + " > " + settlementCountDanger +nl;
-		str += "    set_event_counter " + EVENT_WARNING_NAME + " 1" +nl;
-		str += "  end_if" +nl;
+		str += "  if I_NumberOfSettlements " + factionName + " <= " + settlementsCountWarning + nl;
+		str += "   and I_NumberOfSettlements " + factionName + " > " + settlementCountDanger + nl;
+		str += "    set_event_counter " + EVENT_WARNING_NAME + " 1" + nl;
+		str += "  end_if" + nl;
 
-		str += "  if I_NumberOfSettlements "+factionName + " <= " + settlementCountDanger +nl;
-		str += "   and I_NumberOfSettlements "+factionName + " > " + settlementCountCritical +nl;
-		str += "    set_event_counter " + EVENT_DANGER_NAME + " 1" +nl;
-		str += "  end_if" +nl;
+		str += "  if I_NumberOfSettlements " + factionName + " <= " + settlementCountDanger + nl;
+		str += "   and I_NumberOfSettlements " + factionName + " > " + settlementCountCritical + nl;
+		str += "    set_event_counter " + EVENT_DANGER_NAME + " 1" + nl;
+		str += "  end_if" + nl;
 
-		str += "  if I_NumberOfSettlements "+factionName + " <= " + settlementCountCritical +nl;
-		str += "    set_event_counter " + EVENT_CRITICAL_NAME + " 1" +nl;
-		str += "  end_if" +nl;
+		str += "  if I_NumberOfSettlements " + factionName + " <= " + settlementCountCritical + nl;
+		str += "    set_event_counter " + EVENT_CRITICAL_NAME + " 1" + nl;
+		str += "  end_if" + nl;
 
-		str += "end_monitor" +nl;
+		str += "end_monitor" + nl;
 
 		return str;
 	}
@@ -160,6 +179,7 @@ public class FightForSurvival extends Feature {
 	public UUID getId() {
 		return Id;
 	}
+
 	public static UUID Id = UUID.randomUUID();
 
 	public FightForSurvival(UnitsManager unitsManager) {
