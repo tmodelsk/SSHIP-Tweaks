1b5a04f36d14e4b352817174277856461547551b
Tomasz Modelski <OrdoRerumDev@gmail.com>
2017-10-04 11:35:50 +0200

    UnitsBugFixes : initial commit Mailed Knights & Christian Guard fixes

---
+++ modules/mtwModPatcher.sship/src/tm/mtwModPatcher/sship/features/armyUnits/UnitsBugFixes.java
@@ -0,0 +1,53 @@
+package tm.mtwModPatcher.sship.features.armyUnits;
+
+import lombok.val;
+import tm.mtwModPatcher.lib.common.core.features.Feature;
+import tm.mtwModPatcher.lib.data.exportDescrUnit.ExportDescrUnitTyped;
+
+import java.util.UUID;
+
+/**
+ * Created by tomek on 29.08.2017.
+ */
+public class UnitsBugFixes extends Feature {
+
+	@Override
+	public void executeUpdates() throws Exception {
+		edu = getFileRegisterForUpdated(ExportDescrUnitTyped.class);
+
+		// Mailed Knights dismounted
+		val mailedKnightsCav = edu.loadUnit("Mailed Knights");
+		val mailedKnightsInf = edu.loadUnit("Dismounted Sword Mailed Knights");
+
+		mailedKnightsInf.StatMental = mailedKnightsCav.StatMental;
+
+		// Christian Guard Cavalry
+		val christianGuardCav = edu.loadUnit("Christian Guard");
+		christianGuardCav.StatPri = mailedKnightsCav.StatPri;
+		christianGuardCav.StatSec = mailedKnightsCav.StatSec;
+
+		val christianGuardInf = edu.loadUnit("Dismounted Christian Guard");
+		christianGuardInf.StatPri = mailedKnightsInf.StatPri;
+		christianGuardInf.StatPriArmour = mailedKnightsInf.StatPriArmour;
+		christianGuardInf.StatMental = christianGuardCav.StatMental;
+
+
+	}
+
+	private ExportDescrUnitTyped edu;
+
+	@Override
+	public UUID getId() {
+		return Id;
+	}
+	public static UUID Id = UUID.randomUUID();
+
+	public UnitsBugFixes() {
+		super("Units Bug Fixes");
+
+		addCategory("Units");
+
+		setDescriptionShort("SSHIP units bug fixes");
+		setDescriptionUrl("http://tmsship.wikidot.com/unitsbugfixes");
+	}
+}
