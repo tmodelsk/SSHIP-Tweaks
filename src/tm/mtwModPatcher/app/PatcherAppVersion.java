0bbdf50ff29ccdf352c96f8f47451e3831e202cd
Tomasz Modelski <OrdoRerumDev@gmail.com>
2017-10-29 02:02:03 +0100

    !! Version 1.3 release !!

--- src/tm/mtwModPatcher/app/PatcherAppVersion.java
+++ src/tm/mtwModPatcher/app/PatcherAppVersion.java
@@ -3,6 +3,6 @@ package tm.mtwModPatcher.app;
 /** Patcher App Version info  */
 public class PatcherAppVersion {
 
-	public final static String Version = "1.2";
+	public final static String Version = "1.3.0";
 
 }
